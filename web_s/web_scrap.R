0b123dd1071880c4247ecddd44eb1c979cc6da85
prateek parasher <l00143921@student.lyit.ie>
2018-09-04 22:33:28 +0100

    github link

---
+++ web_s/web_scrap.R
@@ -0,0 +1,67 @@
+#install package 
+install.packages("rvest")
+library(rvest)
+#read link 
+url <- 'https://twitter.com/weatherrte?lang=en'
+web_page <- read_html(url)
+#head & str data
+head(web_page)
+str(web_page)
+#ranking tweet data 
+warning <- html_nodes(web_page,'.tweet-text')
+head(warning, 30)
+#leghth of tweet data
+length(warning)
+data <- html_text(warning)
+#show first 30 tweets 
+head(data, 30)
+#Install and load the required packages
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+# Load the data as a corpus  
+docs <- Corpus(VectorSource(data))
+#Inspect the content of the document
+inspect(docs)
+#Text transformation
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+docs <- tm_map(docs, stemDocument)
+
+#Build a term-document matrix
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 20)
+
+# Generate the Word cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
